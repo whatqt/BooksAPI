16afa21eeb82abe9ee8ce17fea31b9c501713676
whatqt <trutnev.07@list.ru>
2025-02-17 01:50:04 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Books struct {
+	Id                  int    `gorm:"primaryKey"`
+	Name                string `gorm:"type:text"`
+	Author              string `gorm:"type:text"`
+	Quantity_page       int    `gorm:"type:int"`
+	Quantity_of_readers int    `gorm:"type:int"`
+}
+
+type ManagementBooksHttp struct {
+	db *gorm.DB
+}
+
+func (m ManagementBooksHttp) create(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		body, _ := ioutil.ReadAll(r.Body)
+		fmt.Println(string(body))
+		var body_map map[string]string
+		json.Unmarshal(body, &body_map)
+		valid_keys := []string{"name", "author", "quantity_page", "quantity_of_readers"}
+		score := 0
+		for key := range body_map {
+			if valid_keys[score] == key {
+				fmt.Println("valid_keys", valid_keys[score])
+				fmt.Println("key", key)
+				fmt.Println("Успешно\n")
+				score++
+				continue
+			} else {
+				fmt.Println("valid_keys", valid_keys[score])
+				fmt.Println("key", key)
+				fmt.Fprint(w, "Error")
+				break
+			}
+		}
+		quantity_page_int, _ := strconv.Atoi(body_map["quantity_page"])
+		quantity_of_readers_int, _ := strconv.Atoi(body_map["quantity_page"])
+		book := Books{
+			Name:                body_map["name"],
+			Author:              body_map["author"],
+			Quantity_page:       quantity_page_int,
+			Quantity_of_readers: quantity_of_readers_int,
+		}
+		m.db.Create(&book)
+		result := map[string]string{"result": "created"}
+		fmt.Fprint(w, result)
+	} else if r.Method == "GET" {
+		fmt.Fprint(w, "<h1>Hello user!</h1>")
+	}
+}
+
+func (m ManagementBooksHttp) delete(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		body, _ := ioutil.ReadAll(r.Body)
+		var data map[string]string
+		json.Unmarshal(body, &data)
+		fmt.Println(data["id"])
+		err := m.db.Delete(&Books{}, data["id"])
+		if err.Error != nil {
+			fmt.Fprint(w, err.Error)
+			return
+		}
+
+		fmt.Fprint(w, "Book deleted successfully")
+	}
+}
+
+func main() {
+	password, _ := os.LookupEnv("PASSWORD_POSTGRES")
+	dsn := fmt.Sprintf("host=localhost user=postgres dbname=book_accouting password=%s sslmode=disable", password)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Books{})
+	// db.Create(&Books{})
+	base_path := "/api/v1/book/"
+	management_books_http := ManagementBooksHttp{db: db}
+	http.HandleFunc(base_path+"append", management_books_http.create)
+	http.HandleFunc(base_path+"delete", management_books_http.delete)
+	fmt.Println("Сервер запущен")
+	http.ListenAndServe(":8000", nil)
+}
